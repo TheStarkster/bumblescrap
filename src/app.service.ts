ad8ca4c7561a67a70740e0c37bfbed6151d18afb
pawan-sharma <pawansharmaadi@gmail.com>
2024-03-09 01:59:30 +0530

    Your commit message

--- src/app.service.ts
+++ src/app.service.ts
@@ -29,11 +29,12 @@ export class AppService {
   async processDynamicContent(page: puppeteer.Page) {
     // Extracting profile details
     const parentXPath = '/html/body/div/div/div[1]/main/div[2]/div/div/span/div[1]/article/div[1]';
+
     await page.waitForXPath(parentXPath, { timeout: 2 * 60 * 1000 })
       .then(() => console.log("Parent Element found"))
       .catch(err => console.error("Error waiting for element:", err));
 
-      const profiles = await page.$x('/html/body/div/div/div[1]/main/div[2]/div/div/span/div[1]/article/div[1]');
+    const profiles = await page.$x('/html/body/div/div/div[1]/main/div[2]/div/div/span/div[1]/article/div[1]');
     if (profiles.length > 0) {
       const profile = profiles[0]; // Get the first element from the XPath result
 
@@ -55,104 +56,83 @@ export class AppService {
         const studyDetails = studyDetailsElement ? await studyDetailsElement.evaluate(el => el.textContent.trim()) : 'Study details not found';
         console.log(`Image URL: ${profileImageUrl}, Name and Age: ${userNameAndAge}, Verification Status: ${verificationStatus}, Study Details: ${studyDetails}`);
 
-        // Extract paragraphs and unordered lists from the specific section
-        const sectionSelector = '.encounters-story-section.encounters-story-section--about';
-        const contentDivSelector = '.encounters-story-section__content';
+        const paragraphXPath = `${parentXPath}//p`;
+        const unorderedListXPath = `${parentXPath}//ul`;
 
-        const section = await page.$(sectionSelector);
-        if (section) {
-          const contentDiv = await section.$(contentDivSelector);
-          if (contentDiv) {
-            const paragraphs = await contentDiv.$$('p');
-            const unorderedLists = await contentDiv.$$('ul');
+        const paragraphs = await page.$x(paragraphXPath);
+        const unorderedLists = await page.$x(unorderedListXPath);
 
-            if (paragraphs.length || unorderedLists.length) {
-              console.log('Extracted content:');
-              for (const p of paragraphs) {
-                const text = await p.evaluate(element => element.textContent);
-                console.log(`Paragraph: ${text}`);
-              }
-              for (const ul of unorderedLists) {
-                const listItems = await ul.$$('li');
+        console.log('Extracted content:');
+        for (const p of paragraphs) {
+          const text = await p.evaluate(element => element.textContent);
+          console.log(`Paragraph: ${text}`);
+        }
 
-                let _interest = [];
+        for (const ul of unorderedLists) {
+          const listItems = await ul.$x('.//li');  // Using relative XPath to find list items within each ul
+          let _interest = [];
 
-                for (const listItem of listItems) {
-                  const text = await listItem.evaluate(element => element.textContent);
-                  //console.log(`List Item: ${text}`);
+          for (const listItem of listItems) {
+            const text = await listItem.evaluate(element => element.textContent);
+            const images = await listItem.$x('.//img');  // Using relative XPath for images within list item
 
-
-                  // Find images within the list item, assuming each list item has its own set of images
-                  const images = await listItem.$$('img');
-                  if (images.length) {
-                    for (const image of images) {
-                      const imageUrl = await image.evaluate(img => (img as HTMLImageElement).src);
-                      const interestName = this.extractInterest(imageUrl);
-                      _interest.push({ [interestName]: text })
-
-                      // console.log(`Image URL for item: ${imageUrl}`);
-                    }
-                  } else {
-                    console.log('No images found for this list item.');
-                  }
-
-                }
-                console.log(_interest);
-              }
-            } else {
-              console.log('No <p> or <ul> tags found in the specified div.');
-            }
-          } else {
-            console.log('Content div not found in the specified section.');
-          }
-        } else {
-          console.log('Specified section not found.');
-        } 
-      } else {
-        console.log('Profile section not found, moving forward.');
-      }
-      // Handling dynamic divs after the first two fixed divs
-      const allDivs = await profile.$$('div');
-      const fixedDivs = allDivs.slice(0, 2); // First two divs are considered as fixed
-      // Process fixed divs if needed...
-      for (const fixedDiv of fixedDivs) {
-          // Your logic for processing each fixed div
-          console.log('Processing fixed div');
-      }
-
-      const dynamicDivs = allDivs.slice(2); // Remaining divs are considered as dynamic
-      for (const div of dynamicDivs) {
-          const images = await div.$$('img');
-          const headers = await div.$$('h1, h2, h3');
-          const paragraphs = await div.$$('p');
-
-          if (images.length === 2) {
-              console.log('Found 2 photos:');
+            if (images.length) {
               for (const image of images) {
-                  const imageUrl = await image.evaluate(img => (img as HTMLImageElement).src);
-                  console.log('Photo URL:', imageUrl);
+                const imageUrl = await image.evaluate(img => (img as HTMLImageElement).src);
+                const interestName = this.extractInterest(imageUrl);
+                _interest.push({ [interestName]: text });
               }
-          } else if (images.length === 1 && headers.length === 1 && paragraphs.length === 1) {
-              console.log('Found 1 photo and text:');
-              const imageUrl = await images[0].evaluate(img => (img as HTMLImageElement).src);
-              const headerText = await headers[0].evaluate(h => h.textContent);
-              const paragraphText = await paragraphs[0].evaluate(p => p.textContent);
-              console.log(`Photo URL: ${imageUrl}, Header: ${headerText}, Paragraph: ${paragraphText}`);
-          } else if (headers.length === 1 && paragraphs.length === 1) {
-              console.log('Found only text:');
-              const headerText = await headers[0].evaluate(h => h.textContent);
-              const paragraphText = await paragraphs[0].evaluate(p => p.textContent);
-              console.log(`Header: ${headerText}, Paragraph: ${paragraphText}`);
-          } else if (images.length === 1) {
-              console.log('Found 1 photo or location:');
-              const imageUrlOrLocation = await images[0].evaluate(img => (img as HTMLImageElement).src);
-              console.log(`Photo URL or Location: ${imageUrlOrLocation}`);
-          } else {
-              console.log('Unexpected content format.');
+            }
           }
+          console.log(_interest);
+        }
+      } else {
+        console.log('Parent or Profile section not found, moving forward.');
       }
-      
-      
+
+      // Handling dynamic divs after the first two fixed divs
+      // const allDivs = await profile.$$('div');
+     // const fixedDivs = allDivs.slice(0, 2);
+      // Process fixed divs if needed...
+      // for (const fixedDiv of fixedDivs) {
+      //   console.log('Processing fixed div');
+      // }
+      //const allDivs = await profile.$$('div')
+      const dynamicDivsXPath = `${parentXPath}/div[position() >= 3]`;
+    const dynamicDivs = await page.$x(dynamicDivsXPath);
+      //const dynamicDivs = allDivs.slice(2); // Remaining divs are considered as dynamic
+      for (const div of dynamicDivs) {
+        const images = await div.$$('img');
+        const headers = await div.$$('h1, h2, h3');
+        const paragraphs = await div.$$('p');
+
+        if (images.length === 2) {
+          console.log('Found 2 photos:');
+          for (const image of images) {
+            const imageUrl = await image.evaluate(img => (img as HTMLImageElement).src);
+            console.log('Photo URL:', imageUrl);
+          }
+        } else if (images.length === 1 && headers.length === 1 && paragraphs.length === 1) {
+          console.log('Found 1 photo and text:');
+          const imageUrl = await images[0].evaluate(img => (img as HTMLImageElement).src);
+          const headerText = await headers[0].evaluate(h => h.textContent);
+          const paragraphText = await paragraphs[0].evaluate(p => p.textContent);
+          console.log(`Photo URL: ${imageUrl}, Header: ${headerText}, Paragraph: ${paragraphText}`);
+        } else if (headers.length === 1 && paragraphs.length === 1) {
+          console.log('Found only text:');
+          const headerText = await headers[0].evaluate(h => h.textContent);
+          const paragraphText = await paragraphs[0].evaluate(p => p.textContent);
+          console.log(`Header: ${headerText}, Paragraph: ${paragraphText}`);
+        } else if (images.length === 1) {
+          console.log('Found 1 photo or location:');
+          const imageUrlOrLocation = await images[0].evaluate(img => (img as HTMLImageElement).src);
+          console.log(`Photo URL : ${imageUrlOrLocation}`);
+        } else {
+          console.log('Unexpected content format.');
+        }
+      }
+
+
     }
   }
 
